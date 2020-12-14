c5017029ecf2a464f2e051eae41392f8177a51c2
DeepRoy683 <deeproy.training@gmail.com>
2020-12-15 00:09:23 +0530

    connecting to the internet successfully

--- InfonimeApp/app/src/main/java/com/scriptkraft/infonime/MainActivity.kt
+++ InfonimeApp/app/src/main/java/com/scriptkraft/infonime/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.scriptkraft.infonime
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val TAG = "MainActivity"
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://www.google.com"
+        var responseFromRequest: String = ""
+// Request a string response from the provided URL.
+        val stringRequest = StringRequest(Request.Method.GET, url,
+                { response ->
+                    // Display the first 500 characters of the response string.
+                    responseFromRequest =response.substring(0, 500)
+                },
+                { responseFromRequest ="That didn't work!"})
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest)
+    Log.d(TAG,responseFromRequest)
+
     }
 }
